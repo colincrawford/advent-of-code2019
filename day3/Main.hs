eb2004c5aca3b0d1fdd1dd62be1a5711336ffa88
colinlcrawford <ccolin84@gmail.com>
2019-12-16 23:09:21 -0500

    finish day 3 part 2

--- day3/Main.hs
+++ day3/Main.hs
@@ -2,15 +2,16 @@ module Main where
 
 import System.IO
 import qualified Data.Text as T
-import qualified Data.Set as S
+import qualified Data.Map as M
 
 inputFile :: FilePath
 inputFile = "resources/day3/input.txt"
 
 data Move = Up Integer | Down Integer | Left Integer | Right Integer
-type Path = S.Set (Integer, Integer)
+type Path = M.Map (Integer, Integer) Integer
 type Position = (Integer, Integer)
 type Intersection = Position
+type Steps = Integer
 
 strToMove :: String -> Move
 strToMove [] = error "Invalid input movement"
@@ -36,30 +37,23 @@ advance (x, y) move =
         Main.Left amt -> ((x - 1, y), Main.Left $ getNewAmt amt)
         Main.Right amt -> ((x + 1, y), Main.Right $ getNewAmt amt)
 
-addMoveToPath :: (Position, Path) -> Move -> (Position, Path)
+addMoveToPath :: (Position, Path, Steps) -> Move -> (Position, Path, Steps)
 addMoveToPath currentPathPosition (Up 0) = currentPathPosition
 addMoveToPath currentPathPosition (Down 0) = currentPathPosition
 addMoveToPath currentPathPosition (Main.Left 0) = currentPathPosition
 addMoveToPath currentPathPosition (Main.Right 0) = currentPathPosition
-addMoveToPath (pos, path) move =
+addMoveToPath (pos, path, steps) move =
     let (newPos, nextMove) = advance pos move
         (x, y) = newPos
-        updatedPath = S.insert (x, y) path in
-        addMoveToPath (newPos, updatedPath) nextMove
+        newStepCount = steps + 1
+        updatedPath = M.insert (x, y) newStepCount path in
+        addMoveToPath (newPos, updatedPath, newStepCount) nextMove
 
-findIntersections :: [Move] -> [Move] -> S.Set Intersection
+findIntersections :: [Move] -> [Move] -> M.Map Intersection Integer
 findIntersections path1 path2 =
-    let (_, path1Locations) = foldl addMoveToPath ((0, 0), S.empty) path1
-        (_, path2Locations) = foldl addMoveToPath ((0, 0), S.empty) path2 in
-        S.intersection path1Locations path2Locations
-
-manhattanDist :: Position -> Position -> Integer
-manhattanDist (x, y) (x', y') = (abs (x' - x)) + (abs (y' - y))
-
-closest :: [Intersection] -> Integer
-closest [] = error "No intersections found"
-closest intersections =
-    foldr1 min $ (map (\intersection -> manhattanDist (0,0) intersection) intersections)
+    let (_, path1Locations, _) = foldl addMoveToPath ((0, 0), M.empty, 0) path1
+        (_, path2Locations, _) = foldl addMoveToPath ((0, 0), M.empty, 0) path2 in
+        M.intersectionWith (+) path1Locations path2Locations
 
 main :: IO ()
 main = do
@@ -67,7 +61,11 @@ main = do
     content <- hGetContents handle
     let fileLines = lines content
         [path1, path2] = map strToMoves $ take 2 fileLines 
-        intersections = S.toList $ findIntersections path1 path2
-        closestIntersection = closest intersections in
+        intersections = findIntersections path1 path2
+        smallest dist acc = 
+            case acc of
+            Nothing -> Just dist
+            Just a -> if dist > a then Just a else Just dist
+        closestIntersection = M.foldr smallest Nothing intersections in
         putStrLn $ show $ closestIntersection
     hClose handle 
